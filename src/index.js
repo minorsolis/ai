65cb530c9dfab5663e5a4deb03f600d530bc2c10
Minor Solis <minorsolis@gmail.com>
2022-03-05 11:47:05 -0600

    object detection

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Tensor2d from "./ts/Tensor2d";
-import LinearR from "./ts/LinearR";
-import reportWebVitals from "./reportWebVitals";
-import Npl from "./ts/Npl";
+// import LinearR from "./ts/LinearR";
+// import reportWebVitals from "./reportWebVitals";
+// import Npl from "./ts/Npl";
 import Object from "./ts/Object";
 const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
     <Routes>
-      <Route exact path="/ai/linear" element={<LinearR />} />
-      <Route exact path="/ai/npl" element={<Npl />} />
+      {/* <Route exact path="/ai/linear" element={<LinearR />} />
+      <Route exact path="/ai/npl" element={<Npl />} /> */}
       <Route exact path="/ai/object" element={<Object />} />
       <Route exact path="/ai/tensor2d" element={<Tensor2d />} />
       <Route path="/*" element={<App />} />
